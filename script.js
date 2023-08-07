aab00b733796c2bbd5fc980585c82f03d9458d46
jdKheppar <130550913+jdKheppar@users.noreply.github.com>
2023-08-07 18:03:40 -0400

    initial commit
    almost added all the html and css

---
+++ script.js
@@ -0,0 +1,51 @@
+let hamburger = document.getElementById("hamburger-icon");
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+});
+
+window.onscroll = function () {
+  // Check if the hamburger has the "open" class
+  if (!hamburger.classList.contains("open")) {
+    myFunction();
+  }
+};
+
+var header = document.getElementById("nav");
+var sticky = header.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset > sticky) {
+    header.classList.add("sticky");
+  } else {
+    header.classList.remove("sticky");
+  }
+}
+
+
+//For handling carousel
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+  showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+  showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+  let i;
+  let slides = document.getElementsByClassName("mySlides");
+  let dots = document.getElementsByClassName("dot");
+  if (n > slides.length) {slideIndex = 1}    
+  if (n < 1) {slideIndex = slides.length}
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = "none";  
+  }
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(" active", "");
+  }
+  slides[slideIndex-1].style.display = "block";  
+  dots[slideIndex-1].className += " active";
+}
