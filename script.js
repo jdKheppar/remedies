afff47e4ebffe8addf3563a50e5bb412427dbbb0
mohammad junaed <mohammadjunaed858@gmail.com>
2023-08-11 14:37:41 +0500

    new update from thursday

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 let hamburger = document.getElementById("hamburger-icon");
-hamburger.addEventListener("click", () => {
+let burger = document.getElementById("burger");
+
+burger.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+});
+
+bar2.addEventListener("click", () => {
+  hamburger.classList.toggle("open");
+});
+
+bar3.addEventListener("click", () => {
   hamburger.classList.toggle("open");
 });
 
@@ -38,18 +48,64 @@ function showSlides(n) {
   let i;
   let slides = document.getElementsByClassName("mySlides");
   let dots = document.getElementsByClassName("dot");
+  if (n == slides.length) {
+    n = n - 3;
+  }
   if (n > slides.length) {
-    slideIndex = 1;
+    slideIndex = slides.length - 3;
   }
   if (n < 1) {
-    slideIndex = slides.length;
+    slideIndex = 0;
   }
   for (i = 0; i < slides.length; i++) {
     slides[i].style.display = "none";
+    slides[i].style.boxShadow = "none";
   }
   for (i = 0; i < dots.length; i++) {
     dots[i].className = dots[i].className.replace(" active", "");
   }
-  slides[slideIndex - 1].style.display = "block";
-  dots[slideIndex - 1].className += " active";
+
+  slides[slideIndex + 1].style.boxShadow = "0px 0px 5px 8px #f8f8f8";
+  if (window.innerWidth > 771) {
+    slides[slideIndex].style.display = "flex";
+
+    slides[slideIndex + 2].style.display = "flex";
+  }
+
+  slides[slideIndex + 1].style.display = "flex";
+  dots[slideIndex + 1].className += " active";
+}
+
+// // For handling click on hamburger inner icons
+
+function hamburgerInner(n) {
+  let btns = document.getElementsByClassName("hamburger-dropbtn");
+  let hr = document.getElementsByClassName("hamburger-hr");
+
+  for (let i = 0; i < btns.length; i++) {
+    if (btns[i] && btns[i].style) {
+      btns[i].style.display = "none";
+    }
+    if (hr[i] && hr[i].style) {
+      hr[i].style.display = "none";
+    }
+  }
+  if (n == 1) {
+    let healthGoal = document.getElementById("hamburger-health-goal-inner");
+    healthGoal.style.display = "flex";
+    healthGoal.style.color = "#2e6e66";
+    console.log("Hamburger Clikced");
+  }
+  if (n == 2) {
+    let healthGoal = document.getElementById("hamburger-product-type-inner");
+    healthGoal.style.display = "flex";
+    healthGoal.style.color = "#2e6e66";
+    console.log("Hamburger Clikced");
+  }
+  if (n == 3) {
+    let healthGoal = document.getElementById("hamburger-brands-inner");
+    healthGoal.style.display = "flex";
+    healthGoal.style.color = "#2e6e66";
+    console.log("Hamburger Clikced");
+  }
 }
