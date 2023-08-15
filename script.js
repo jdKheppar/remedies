86825d37bf70c662c98c67ea6df37f60001f069a
junaidK <130550913+jdKheppar@users.noreply.github.com>
2023-08-15 16:07:55 -0700

    tuesday

--- script.js
+++ script.js
@@ -6,6 +6,7 @@ let burger = document.getElementById("burger");
 let healthGoal = document.getElementById("hamburger-health-goal-inner");
 let productType = document.getElementById("hamburger-product-type-inner");
 let brands = document.getElementById("hamburger-brands-inner");
+let cartContainer = document.getElementById("cart-container");
 
 let main = document.getElementById("main");
 
@@ -17,13 +18,16 @@ burger.addEventListener("click", () => {
   if (hamburger.classList.contains("open")) {
     hamburger.classList.remove("open");
     main.style.display = "block";
-    healthGoal.style.display = "none";
-    productType.style.display = "none";
+    healthGoal.style.transform = 'translateX(-100%)';
+    productType.style.transform = 'translateX(-100%)';
+    brands.style.transform = 'translateX(-100%)';
     mobileMenu.style.display = "none";
+    cartContainer.style.display = "block";
   } else {
     hamburger.classList.add("open");
     mobileMenu.style.display = "flex";
     main.style.display = "none";
+    cartContainer.style.display = "none";
   }
 });
 
@@ -49,7 +53,13 @@ let healthGoalRight = document.getElementById("health-goal-right");
 function myFunction() {
   if (window.scrollY > sticky) {
     header.classList.add("sticky");
-    dropDown.style.height = "7.6vh";
+    if (window.innerWidth > 770 && window.innerWidth < 907) {
+      dropDown.style.height = "16vh";
+    }
+    else {
+      dropDown.style.height = "7.6vh";
+    }
+
     // if (!mainDDUL.style.contains("wrap")) {
     //   healthGoalRight.style.gridTemplateColumns = "auto";
     // } else {
@@ -62,84 +72,29 @@ function myFunction() {
   }
 }
 
-//For handling carousel
-let slideIndex = 1;
-showSlides(slideIndex);
 
-function plusSlides(n) {
-  showSlides((slideIndex += n));
-}
-
-function currentSlide(n) {
-  showSlides((slideIndex = n));
-}
-
-function showSlides(n) {
-  //Use transitions here instead of display none and flex
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-    slides[i].style.boxShadow = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-
-  if (window.innerWidth > 771) {
-    if (n === 10) {
-      slides[0].style.display = "flex";
-    }
-    else {
-      slides[n].style.display = "flex";
-    }
-    console.log("This is nothing");
-    if (n === 1) {
-      slides[9].style.display = "flex";
-    }
-    else {
-      slides[n - 2].style.display = "flex";
-    }
-
-  }
-  slides[n - 1].style.boxShadow = "0px 0px 5px 8px #f8f8f8";
-  slides[n - 1].style.display = "flex";
-  dots[n - 1].className += " active";
-
-}
 
 // // For handling click on hamburger inner icons
 
 function hamburgerInner(n) {
   if (n == 1) {
     mobileMenu.style.display = "none";
-
-    healthGoalInner.style.display = "flex";
-    healthGoalInner.style.flexDirection = "column";
-    healthGoalInner.style.color = "#2e6e66";
-    console.log("Hamburger Clikced");
+    healthGoalInner.style.transform = 'translateX(0)';
   }
   if (n == 2) {
     mobileMenu.style.display = "none";
-
-    productType.style.display = "flex";
-    productType.style.flexDirection = "column";
-    productType.style.color = "#2e6e66";
+    productType.style.transform = 'translateX(0)';
   }
   if (n == 3) {
     mobileMenu.style.display = "none";
-    brands.style.display = "flex";
-    brands.style.flexDirection = "column";
-    brands.style.color = "#2e6e66";
-
+    brands.style.transform = 'translateX(0)';
   }
 }
 
 function hamburgerInnerBtn() {
   console.log("I am clicked");
   mobileMenu.style.display = "flex";
-  healthGoalInner.style.display = "none";
-  productType.style.display = "none";
-  brands.style.display = "none";
+  healthGoal.style.transform = 'translateX(100%)';
+  productType.style.transform = 'translateX(100%)';
+  brands.style.transform = 'translateX(100%)';
 }
